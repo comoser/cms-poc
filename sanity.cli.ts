0af8396235cfe7843e64d244f487277eb393f311
comoser <david.alecrim1@gmail.com>
2023-03-16 11:43:51 +0000

    graphql

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,5 +4,13 @@ export default defineCliConfig({
   api: {
     projectId: 'zd6eo5jz',
     dataset: process.env.SANITY_STUDIO_API_DATASET as string
-  }
+  },
+  graphql: [
+    {
+      playground: false,
+      tag: 'api',
+      workspace: 'default',
+      id: 'schema-experiment',
+    },
+  ]
 })
