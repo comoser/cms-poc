a40fe5752e9a83bfef0cb1c5ebf027ace89b15ef
comoser <david.alecrim1@gmail.com>
2023-03-20 12:05:45 +0000

    add studio id

--- schemas/page.ts
+++ schemas/page.ts
@@ -4,6 +4,11 @@ export default {
   type: 'document',
   fields: [
     {
+      name: 'studioPageId',
+      title: 'Studio Page Id',
+      type: 'string'
+    },
+    {
       name: 'pageTitle',
       title: 'Page Title',
       type: 'localeRichTextEditor',
